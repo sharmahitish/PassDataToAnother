cff1b1a2baf268cc6fbf5ea8dd598446b3fd954f
Hitish <hitish1.sharma1@gmail.com>
2020-10-09 19:59:13 +0530

    Initial Commit

---
+++ PassDataToAnotherTests/PassDataToAnotherTests.swift
@@ -0,0 +1,34 @@
+//
+//  PassDataToAnotherTests.swift
+//  PassDataToAnotherTests
+//
+//  Created by kunal sharma on 10/9/20.
+//  Copyright © 2020 Hitish sharma. All rights reserved.
+//
+
+import XCTest
+@testable import PassDataToAnother
+
+class PassDataToAnotherTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
